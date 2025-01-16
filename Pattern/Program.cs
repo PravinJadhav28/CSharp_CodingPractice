aed4cf6a3331e4182387918063899d7ce99b9a07
lenovo <lenovo@LAPTOP-RD6BG50C>
2025-01-16 22:56:03 +0530

    Star Pattern

---
+++ Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
