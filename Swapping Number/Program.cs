35e991bb840ba0c9b1ff9dc97539ba3949517013
lenovo <lenovo@LAPTOP-RD6BG50C>
2025-01-17 23:33:45 +0530

    swapping number without using third variable

---
+++ Swapping Number/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 22;
+        int b = 40;
+        Console.WriteLine($"a : {a} b : {b}");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine($"After swap a: {a}");
+        Console.WriteLine($"After swap b : {b}");
+
+        Console.ReadLine();
+    }
+}
