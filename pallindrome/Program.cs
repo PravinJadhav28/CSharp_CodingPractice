267b3aeda9c7c1102447e780e416a71319908805
lenovo <lenovo@LAPTOP-RD6BG50C>
2025-01-20 16:07:32 +0530

    pallindrome code file

---
+++ pallindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string choose = "";
+        do
+        {
+            string word = "";
+            string rev = "";
+            Console.WriteLine("Enter word");
+            word = Console.ReadLine();
+            for (var i = word.Length - 1; i >= 0; i--)
+            {
+                rev += word[i];
+            }
+            if (word == rev)
+            {
+                Console.WriteLine($"{word} : Pallindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{word} : Not Pallindrome");
+            }
+            Console.WriteLine("Do you want to continue?");
+            choose = Console.ReadLine().ToUpper();
+        } while (choose == "Y" || choose == "YES");
+
+        Console.WriteLine("Thank You");
+        Console.WriteLine("Please leave the Page");
+        Console.ReadLine();
+    }
+}
