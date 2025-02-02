eeef4946e4c3db3302a396d11124941c60ca4611
lenovo <jadhavpravin195@gmail.com>
2025-02-03 00:18:54 +0530

    Remove special character from given string

---
+++ RemoveSpecialCharacter/Program.cs
@@ -0,0 +1,24 @@
+﻿
+   // string inputString = "Hello, World! 123 @#$%";
+    Console.Write("Plz Enter special chracter string: ");
+    string name = Console.ReadLine();
+   string outputString = RemoveSpecialCharacters(name);
+    Console.WriteLine($"After Removal Special Character:{outputString}");
+
+
+ static string RemoveSpecialCharacters(string inputString)
+{
+    string result = "";
+    foreach (char c in inputString)
+    {
+        if ((c >= 'a' && c <= 'z') ||
+         (c >= 'A' && c <= 'Z') ||
+          (c >= '0' && c <= '9') ||
+           c == ' ')
+        {
+            result += c;
+        }
+    }
+    return result;
+    Console.ReadLine();
+}
--- Swapping Number/obj/Debug/net8.0/Swapping Number.AssemblyInfo.cs
+++ Swapping Number/obj/Debug/net8.0/Swapping Number.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Swapping Number")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed4cf6a3331e4182387918063899d7ce99b9a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267b3aeda9c7c1102447e780e416a71319908805")]
 [assembly: System.Reflection.AssemblyProductAttribute("Swapping Number")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Swapping Number")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
